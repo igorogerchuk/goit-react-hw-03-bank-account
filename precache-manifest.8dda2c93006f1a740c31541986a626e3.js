6ba0305d9dd19f3071820912f9e3a06ed04496df
Igor <igor.ogerchuk@gmail.com>
2019-11-03 00:18:57 +0200

    Updates

---
+++ precache-manifest.8dda2c93006f1a740c31541986a626e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57ac0dfd885d607e96c214c67080999",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "1be7b0d15b2ef69d1082",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "1af03c99dbe77dad8ea7",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.864048af.chunk.css"
+  },
+  {
+    "revision": "1be7b0d15b2ef69d1082",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.51c0b686.chunk.js"
+  },
+  {
+    "revision": "1af03c99dbe77dad8ea7",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.ec143974.chunk.js"
+  },
+  {
+    "revision": "9442ab920daa0b9f3f5e",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.8518e471.js"
+  }
+]);
